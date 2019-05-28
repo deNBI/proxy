ef7a7477d0e9f1a845bcfca163d287903508ade2
pbelmann <pbelmann@cebitec.uni-bielefeld.de>
2019-05-28 20:47:31 +0200

    fix(acme): add update.sh to correct path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-/root/.acme.sh/acme.sh --install-cert -d portal-dev.denbi.de --cert-file /tmp/cert.pem --key-file /tmp/key.pem --fullchain-file /tmp/fullchain.pem --reloadcmd "./update.sh" --debug
+/root/.acme.sh/acme.sh --install-cert -d portal-dev.denbi.de --cert-file /tmp/cert.pem --key-file /tmp/key.pem --fullchain-file /tmp/fullchain.pem --reloadcmd "/update.sh" --debug
 
 ./docker-entrypoint.sh "$@"
