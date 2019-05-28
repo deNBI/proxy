4f969e7397e039ca3c7e4ffa7e222434173b982a
pbelmann <pbelmann@cebitec.uni-bielefeld.de>
2019-05-28 20:55:29 +0200

    fix(haproxy): fix cert name

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Concatenate new cert files, with less output (avoiding the use tee and its output to stdout)
-bash -c "cat /tmp/fullchain.pem /tmp/key.pem > /usr/local/etc/haproxy/haproxy.pem"
+bash -c "cat /tmp/fullchain.pem /tmp/key.pem > /etc/haproxy/ssl/cert.pem"
 
 # Reload  HAProxy
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg -D -W
