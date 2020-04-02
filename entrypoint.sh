b29d3ee0f2fbbc13f10357992677b4cd8e7cb529
Ewgenij Katchko <ekatchko@techfak.uni-bielefeld.de>
2020-04-02 22:22:40 +0200

    remove issue command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-/root/.acme.sh/acme.sh --issue -d portal-dev.denbi.de --standalone --httpport 8888
-
 /root/.acme.sh/acme.sh --install-cert -d portal-dev.denbi.de --cert-file /tmp/cert.pem --key-file /tmp/key.pem --fullchain-file /tmp/fullchain.pem --reloadcmd "/update.sh" --debug
 
 ./docker-entrypoint.sh "$@"
